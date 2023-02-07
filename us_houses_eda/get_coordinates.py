6ee7ea5168513792e2e1200a3b4ca00e42979ca7
hithik7336 <sharmahrithik432gmail.com>
2023-02-07 21:30:51 +0530

    added check for None Coordinates

--- us_houses_eda/get_coordinates.py
+++ us_houses_eda/get_coordinates.py
@@ -9,6 +9,8 @@ def get_latitude(location: str) -> float:
     """get latitude of location"""
 
     latitude = GEOCODER.geocode(location).latitude
+    if latitude is None:
+        return None
     return latitude
 
 
@@ -16,4 +18,6 @@ def get_longitude(location: str) -> float:
     """get longitude of location"""
 
     longitude = GEOCODER.geocode(location).longitude
+    if longitude is None:
+        return None
     return longitude
