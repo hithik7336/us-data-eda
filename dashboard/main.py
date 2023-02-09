6b979e688490eeb374108d71482a0a48e009053a
hithik7336 <sharmahrithik432gmail.com>
2023-02-09 21:58:39 +0530

    pylint fixes

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,7 +5,8 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div(id='outer-div', children=[], style={'border':'Solid 5px Black', 'border-radius': '3px'})
+app.layout = html.Div(id='outer-div', children=[],
+                      style={'border':'Solid 5px Black', 'border-radius': '3px'})
 
 
 if __name__ == "__main__":
