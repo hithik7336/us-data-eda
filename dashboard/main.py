b3c3b8906461494732500b5bc501697496f8403f
hithik7336 <sharmahrithik432gmail.com>
2023-02-11 13:51:58 +0530

    used style dicts from style file

--- dashboard/main.py
+++ dashboard/main.py
@@ -3,10 +3,12 @@
 import dash
 import dash_html_components as html
 
+from style_dicts import DASBOARD_TOPIC_DICT, LAYOUT_SYTLE_DICT
+
 app = dash.Dash()
 
-app.layout = html.Div(id='outer-div', children=[],
-                      style={'border':'Solid 5px Black', 'border-radius': '3px'})
+app.layout = html.Div(id='outer-div', children=[html.H1('U.S.A Houses Dashboard', style=DASBOARD_TOPIC_DICT)],
+                      style=LAYOUT_SYTLE_DICT)
 
 
 if __name__ == "__main__":
