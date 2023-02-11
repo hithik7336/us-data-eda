22c1b0d903a94877d866f82f3e3617d3617215a8
hithik7336 <sharmahrithik432gmail.com>
2023-02-11 15:11:59 +0530

    added dropdowns and style changes

--- dashboard/main.py
+++ dashboard/main.py
@@ -2,13 +2,19 @@
 
 import dash
 import dash_html_components as html
+import dash_core_components as dcc
 
-from style_dicts import DASBOARD_TOPIC_DICT, LAYOUT_SYTLE_DICT
+from style_dicts import DASBOARD_TOPIC_DICT, LEFT_DIV_STYLE, RIGHT_DIV_STYLE
 
 app = dash.Dash()
 
-app.layout = html.Div(id='outer-div', children=[html.H1('U.S.A Houses Dashboard', style=DASBOARD_TOPIC_DICT)],
-                      style=LAYOUT_SYTLE_DICT)
+app.layout = html.Div(id='outer-div', children=[html.H1('U.S.A Houses Dashboard', style=DASBOARD_TOPIC_DICT),
+
+                                                html.Div(id='left-div', children=[dcc.Dropdown(), html.Hr(), dcc.Dropdown()],
+                                                         style=LEFT_DIV_STYLE),
+
+                                                html.Div(id='right-div', children=[dcc.Dropdown(), html.Hr(), dcc.Dropdown()],
+                                                         style=RIGHT_DIV_STYLE)])
 
 
 if __name__ == "__main__":
