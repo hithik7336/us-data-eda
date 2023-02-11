e3ba06dc669bc3f943fa9129111748806db83fa1
hithik7336 <sharmahrithik432gmail.com>
2023-02-11 16:41:29 +0530

    added graph border styles

--- dashboard/main.py
+++ dashboard/main.py
@@ -4,22 +4,26 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 
-from style_dicts import DASBOARD_TOPIC_DICT, LEFT_DIV_STYLE, RIGHT_DIV_STYLE
+from style_dicts import DASBOARD_TOPIC_DICT, LEFT_DIV_STYLE, RIGHT_DIV_STYLE, GRAPH_STYLE
 
 app = dash.Dash()
 
 app.layout = html.Div(id='outer-div', children=[html.H1('U.S.A Houses Dashboard', style=DASBOARD_TOPIC_DICT),
 
-                                                html.Div(id='left-div', children=[dcc.Dropdown(), html.Hr(), 
+                                                html.Div(id='left-div', children=[dcc.Dropdown(), html.Hr(),
                                                                                   dcc.Dropdown(),
-                                                                                  dcc.Graph(),
-                                                                                  dcc.Graph()],
+                                                                                  html.Hr(),
+                                                                                  dcc.Graph(id='graph-1', style=GRAPH_STYLE),
+                                                                                  html.Hr(),
+                                                                                  dcc.Graph(id='graph-2', style=GRAPH_STYLE)],
                                                          style=LEFT_DIV_STYLE),
 
-                                                html.Div(id='right-div', children=[dcc.Dropdown(), html.Hr(), 
+                                                html.Div(id='right-div', children=[dcc.Dropdown(), html.Hr(),
                                                                                    dcc.Dropdown(),
-                                                                                   dcc.Graph(),
-                                                                                   dcc.Graph()],
+                                                                                   html.Hr(),
+                                                                                   dcc.Graph(id='graph-3', style=GRAPH_STYLE),
+                                                                                   html.Hr(),
+                                                                                   dcc.Graph(id='graph-4', style=GRAPH_STYLE)],
                                                          style=RIGHT_DIV_STYLE)])
 
 
--- dashboard/style_dicts.py
+++ dashboard/style_dicts.py
@@ -1,7 +1,10 @@
 """module contains style dicts for dashboard"""
 
-LAYOUT_SYTLE_DICT = {'border':'Solid 5px rgb(34, 106, 12)', 'border-radius': '3px', 'background-color':'Black'}
-DASBOARD_TOPIC_DICT = {'text-align':'center', 'color':'rgb(34, 106, 12)', 'font-family':'tahoma', 'text-decoration':'underline'}
+LAYOUT_SYTLE_DICT = {'border': 'Solid 5px rgb(34, 106, 12)', 'border-radius': '3px', 'background-color': 'Black'}
+DASBOARD_TOPIC_DICT = {'text-align': 'center', 'color': 'rgb(34, 106, 12)', 'font-family': 'tahoma', 
+                       'text-decoration': 'underline'}
 
-LEFT_DIV_STYLE = {'border':'Solid 2px Black', 'float':'left', 'padding':'5px', 'width':'49%', 'height':'99%'}
-RIGHT_DIV_STYLE = {'border':'Solid 2px Black', 'float':'right', 'padding':'5px', 'width':'49%', 'height':'99%'}
+LEFT_DIV_STYLE = {'border': 'Solid 5px Black', 'float': 'left', 'padding': '5px', 'width': '49%', 'height': '99%'}
+RIGHT_DIV_STYLE = {'border': 'Solid 5px Black', 'float': 'right', 'padding': '5px', 'width': '49%', 'height': '99%'}
+
+GRAPH_STYLE = {'border': 'Solid 5px Black', 'border-radius': '3px'}
