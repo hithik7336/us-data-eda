26183dd2d47d2217c86f850e1cdb679de7445c02
hithik7336 <sharmahrithik432gmail.com>
2023-02-11 20:47:39 +0530

    pylint fixes

--- dashboard/data.py
+++ dashboard/data.py
@@ -9,9 +9,9 @@ us_states_df = gpd.read_file('../usa-states-shapefile/cb_2018_us_state_500k.shp'
 
 def get_average_price_by_state(state):
     """getting average house price in each state"""
-    
+
     by_state = us_houses_geo_df.groupby('state')
-    
+
     if state not in us_houses_geo_df['state'].unique():
         av_house_price = 0
     else:
@@ -22,5 +22,4 @@ def get_average_price_by_state(state):
 
 def single_state_house_price(state: str) -> go.figure.Figure:
     """plots single state plot for average house price"""
-    pass
-     
+    
