dd2bb87cea8c5fe50256189d0251e70354d76aae
hithik7336 <sharmahrithik432gmail.com>
2023-02-11 13:51:25 +0530

    added separate file for style dicts

---
+++ dashboard/style_dicts.py
@@ -0,0 +1,4 @@
+"""module contains style dicts for dashboard"""
+
+LAYOUT_SYTLE_DICT = {'border':'Solid 5px rgb(34, 106, 12)', 'border-radius': '3px', 'background-color':'Black'}
+DASBOARD_TOPIC_DICT = {'text-align':'center', 'color':'rgb(34, 106, 12)', 'font-family':'tahoma', 'text-decoration':'underline'}
