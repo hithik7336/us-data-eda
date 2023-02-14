4136569624fc540ec8fb9c49d0243ea20d7aca38
hithik7336 <sharmahrithik432gmail.com>
2023-02-14 21:50:19 +0530

    removed unnecessery codes

--- dashboard/main.py
+++ dashboard/main.py
@@ -4,6 +4,8 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 
+from data import get_average_price_by_state
+
 from style_dicts import DASBOARD_TOPIC_DICT, LEFT_DIV_STYLE, RIGHT_DIV_STYLE, GRAPH_STYLE
 
 app = dash.Dash()
@@ -12,7 +14,7 @@ app.layout = html.Div(id='outer-div', children=[html.H1('U.S.A Houses Dashboard'
                                                         style=DASBOARD_TOPIC_DICT),
 
                                                 html.Div(id='left-div',
-                                                         children=[dcc.Dropdown(),
+                                                         children=[dcc.Dropdown(id='us-states'),
                                                                     html.Hr(),
                                                                     dcc.Dropdown(),
                                                                     html.Hr(),
@@ -34,7 +36,6 @@ app.layout = html.Div(id='outer-div', children=[html.H1('U.S.A Houses Dashboard'
                                                                     dcc.Graph(id='graph-4',
                                                                               style=GRAPH_STYLE)],
                                                          style=RIGHT_DIV_STYLE)])
-
 
 if __name__ == "__main__":
     app.run_server(debug=True)
